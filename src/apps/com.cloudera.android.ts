26b3e45bf95c40c8ff31b54e40001bbbb7dca229
pyh20 <pyh20@users.noreply.github.com>
2026-04-06 13:21:57 +0800

    fix: 慧生活 798 精准匹配跳过按钮（只匹配右上角无倒计时的跳过）

--- src/apps/com.cloudera.android.ts
+++ src/apps/com.cloudera.android.ts
@@ -15,19 +15,8 @@ export default defineGkdApp({
       rules: [
         {
           activityIds: 'com.cloudera.android.ui.activity.SplashActivity',
-          matches: [
-            '[text*="跳过" && text.length<=10]',
-            '[vid*="skip" || vid*="count" || vid*="jump"]',
-          ],
+          matches: '[text="跳过" && text.length<=4][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/18375410',
-        },
-        {
-          matches: '[text*="跳过" && text.length<=10][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/18375410',
-        },
-        {
-          matches: '[text*="跳过" && text*="s" && text.length<=10][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/18375415',
         },
       ],
     },
@@ -55,23 +44,13 @@ export default defineGkdApp({
       rules: [
         {
           activityIds: 'com.cloudera.android.ui.activity.MainActivity',
-          matches: '[text="京东" || text="查看详情"] + [vid="close_btn" || id="close_btn" || text="×"]',
+          matches: '[text="京东"] +2 [text="×"][visibleToUser=true][childCount=0]',
           snapshotUrls: 'https://i.gkd.li/i/18375412',
         },
         {
           activityIds: 'com.cloudera.android.ui.activity.MainActivity',
-          matches: '[text^="来 oppo 软件商店"] + [vid="close_btn" || id="close_btn" || text="×"]',
-          snapshotUrls: 'https://i.gkd.li/i/18375413',
-        },
-        {
-          activityIds: 'com.cloudera.android.ui.activity.MainActivity',
-          matches: '[text="立即参与" || text="立即领取"] + [text="查看"] + [text="×"][visibleToUser=true]',
+          matches: '[text="立即参与" || text="立即领取"] + [text="查看"] + [text="×"][visibleToUser=true][childCount=0]',
           snapshotUrls: 'https://i.gkd.li/i/18375416',
-        },
-        {
-          activityIds: 'com.cloudera.android.ui.activity.MainActivity',
-          matches: '[text="京东"] <<n [text="×"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/18375417',
         },
       ],
     },
